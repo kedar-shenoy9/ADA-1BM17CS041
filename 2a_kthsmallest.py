4f01e317a1806111df3d42b4f7360e9742fee884
kedar-shenoy9 <kedarshenoy.cs17@bmsce.ac.in>
2019-08-22 14:26:44 +0530

    Second lab

---
+++ 2a_kthsmallest.py
@@ -0,0 +1,16 @@
+def kthsmall(arr, k):
+	for i in range(0, k):
+		small_pos = i
+		for j in range(i+1, len(arr)):
+			if arr[j] < arr[small_pos]:
+				small_pos = j
+		temp = arr[small_pos]
+		arr[small_pos] = arr[i]
+		arr[i] = temp
+	return arr[k-1]
+
+
+
+l = list(map(int, input("Enter the list of numbers ").split()))
+k = int(input("Enter the value of k"))
+print(kthsmall(l, k))
---
+++ 2b_klargest.py
@@ -0,0 +1,17 @@
+def klarge(arr, k):
+	for i in range(0, k):
+		big_pos = i
+		for j in range(i+1, len(arr)):
+			if arr[j] > arr[big_pos]:
+				big_pos = j
+		temp = arr[big_pos]
+		arr[big_pos] = arr[i]
+		arr[i] = temp
+	return arr[0:k]
+
+
+
+l = list(map(int, input("Enter the list of numbers ").split()))
+k = int(input("Enter the value of k"))
+newL = klarge(l, k)
+print(newL)
